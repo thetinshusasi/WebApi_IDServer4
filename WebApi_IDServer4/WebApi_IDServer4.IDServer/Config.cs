a3b33a10501e0c739c428c5058932d55e0734476
Tinshu Sasi <thetinshusasi@gmail.com>
2020-03-28 20:30:42 +0530

    temp id server project added

---
+++ WebApi_IDServer4/WebApi_IDServer4.IDServer/Config.cs
@@ -0,0 +1,42 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_IDServer4.IDServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("MyDBCustomerApi", "Customers in MyDB")
+
+            };
+
+        }
+
+        public static IEnumerable<Client> GetAllClients()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId="client",
+                    AllowedGrantTypes= GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes =
+                    {
+                        "MyDBCustomerApi"
+                    }
+
+                }
+            };
+        }
+    }
+}
